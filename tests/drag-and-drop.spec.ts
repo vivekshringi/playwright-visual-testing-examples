3527d6fa9f8a9a5afc9489e33263df6119ad5c05
vivekshringi <shringi.vivek@gmail.com>
2023-11-10 22:15:27 +0100

    util function usage

--- tests/drag-and-drop.spec.ts
+++ tests/drag-and-drop.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect, Page} from '@playwright/test';
+import { gotoNagarro } from '../util/util';
 
 test.beforeEach(async ({page})=> {
   await page.setViewportSize({ width: 1900, height: 1000 });
-  await page.goto('https://www.nagarro.com/en', { waitUntil: 'networkidle' });
-  await page.click('#hs-eu-confirmation-button');
-  await page.setDefaultTimeout(5000);
+  await gotoNagarro(page);
+  page.setDefaultTimeout(5000);
 })
 
 let drag = async (page:Page, source:string, destination:string) => {
@@ -12,7 +12,7 @@ let drag = async (page:Page, source:string, destination:string) => {
   const left = (await leftbox?.boundingBox())!;
   const rightbox = await page.$(destination);
   const right = (await rightbox?.boundingBox())!;
-  await page.waitForTimeout(50);
+  await page.waitForTimeout(500);
   await page.mouse.move(right.x + right.width/2 ,right.y + right.height/2);
   await page.mouse.down();
   await page.mouse.move(left.x + left.width/2 ,left.y + left.height/2);
@@ -22,9 +22,11 @@ let drag = async (page:Page, source:string, destination:string) => {
 
 test.only('test 1 - fullscreen', async ({ page}) => {
    await page.locator('#StorySliderID').scrollIntoViewIfNeeded();
+   expect (page.locator('#StorySliderID')).toBeVisible();
    for(let i=0;i<4;i++){
     await expect(page.locator('.featured-story-wrapper')).toHaveScreenshot(`slide${i}.png`,{maxDiffPixelRatio:.02});
     await drag(page,'.story-list.active-slide .left-section','.story-list.active-slide .right-section');
    }
+   await page.waitForTimeout(500);
    await expect(page.locator('.featured-story-wrapper')).toHaveScreenshot(`slide0.png`,{maxDiffPixelRatio:.05});
 });
