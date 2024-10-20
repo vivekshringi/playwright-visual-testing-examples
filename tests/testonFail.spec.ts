5727b92c3b8a2cad6f854b1abb69fd3f4c2c4750
vivekshringi <shringi.vivek@gmail.com>
2024-10-20 17:36:58 +0200

    added testOnFailScenario

--- tests/testonFail.spec.ts
+++ tests/testonFail.spec.ts
@@ -7,16 +7,11 @@ let testURL = "https://coffee-cart.app";
 test.describe("mock network api", () => {
   let homePage: HomePage;
   test.beforeEach(async ({ page }) => {
-    await page.route("*/**/list.json", async (route) => {
-      const json = mockData;
-      await route.fulfill({ json });
-    });
-    await page.goto(testURL);
-    console.log(mockData);
     homePage = new HomePage(page);
   });
 
   test('if coffee count is correct @coffeeMenu', async () => {
+    test.fail();
     await expect(homePage.coffee).toHaveCount(1);
     expect(await homePage.getCoffeeCount()).toEqual(1);
   });
